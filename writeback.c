103360d5c764eed34aaf03bbeeca1562c3420e80
finals <finals@126.com>
2018-03-12 11:26:03 +0800

    writeback trigger gc modify condition

--- writeback.c
+++ writeback.c
@@ -671,7 +671,7 @@ static void trigger_bucket_gc(struct cache_set *c, struct cached_dev *dc)
         goto trigger;
     } else if (c->gc_stats.in_use < 70 && now - c->gc_stats.last_trigger_gc_time > 300 * NSEC_PER_SEC) {
         goto trigger;
-    } else (now - c->gc_stats.last_trigger_gc_time > 60 * NSEC_PER_SEC) {
+    } else if (now - c->gc_stats.last_trigger_gc_time > 60 * NSEC_PER_SEC) {
         goto trigger;
     }
     
